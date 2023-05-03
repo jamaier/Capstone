69115c330e630ab934f7770fc4f845d8918d4004
Jacob Maier <jacobamaier@gmail.com>
2023-05-02 17:04:31 -0700

    Add formatting change for readability PersonalPref

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
-import React from "react"
-import Header from "../components/Header"
+import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <Header label="header" />
-  )
+    <>
+      <Header label="header" />
+    </>
+  );
 }
