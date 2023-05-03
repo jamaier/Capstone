15ef27661b7d20752256fd7e1ee7132d92d56820
Jacob Maier <jacobamaier@gmail.com>
2023-05-02 17:03:18 -0700

    Add more Layout styling

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import Sidebar from './Layout/Sidebar';
+
+import FollowBar from '@/components/layout/FollowBar';
+import Sidebar from '@/components/layout/Sidebar';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="h-screen bg-black">
-      <Sidebar />
-      <div>
-        {children}
+      <div className="container h-full max-w-6xl mx-auto xl:px-30">
+        <div className="grid h-full grid-cols-4">
+          <Sidebar />
+          <div
+            className="
+              col-span-3 
+              lg:col-span-2 
+              border-x-[1px] 
+              border-neutral-800
+          "
+          >
+            {children}
+          </div>
+          <FollowBar />
+        </div>
       </div>
     </div>
   );
