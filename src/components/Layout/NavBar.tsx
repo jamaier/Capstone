0c8c4029fe165633186a64fc54e7d4d82c5dc563
Jacob Maier <jacobamaier@gmail.com>
2023-05-03 15:35:10 -0700

    update current user info for logout functionality

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,3 +1,5 @@
+import { signOut } from 'next-auth/react';
+
 import { BsBellFill, BsHouseFill } from 'react-icons/bs';
 import { FaUser } from 'react-icons/fa';
 import { BiLogOut } from 'react-icons/bi';
@@ -5,7 +7,11 @@ import { BiLogOut } from 'react-icons/bi';
 import SidebarItem from '@/components/layout/NavBarItem';
 import SidebarLogo from '@/components/layout/NavBarLogo';
 
+import useCurrentUser from '@/hooks/useCurrentUser';
+
 const Sidebar = () => {
+  const { data: currentUser } = useCurrentUser();
+
   const items = [
     {
       label: 'Home',
@@ -32,18 +38,20 @@ const Sidebar = () => {
       <div className="flex flex-col items-end">
         <div className="space-y-2 lg:w-[175px]">
           <SidebarLogo />
-          <div className='pt-10'>
-          {items.map((item) => (
+          <div className="pt-10">
+            {items.map((item) => (
               <SidebarItem
                 key={item.href}
                 href={item.href}
                 label={item.label}
                 icon={item.icon}
               />
-              ))}
-              </div>
+            ))}
+          </div>
           <div className="absolute bottom-0 z-10 flex logout-container py-11">
-            <SidebarItem onClick={() => {}} icon={BiLogOut} label="logOut" />
+            {currentUser && (
+              <SidebarItem onClick={() => signOut()} icon={BiLogOut} label="logOut" />
+            )}
           </div>
         </div>
       </div>
--- src/components/Layout/NavBarItem.tsx
+++ src/components/Layout/NavBarItem.tsx
@@ -1,3 +1,6 @@
+import { useRouter } from 'next/router';
+import { useCallback } from 'react';
+
 import { IconType } from 'react-icons';
 
 interface SidebarItemProps {
@@ -13,8 +16,17 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
   icon: Icon,
   onClick
 }) => {
+  const router = useRouter();
+  const handleClick = useCallback(() => {
+    if (onClick) {
+      return onClick();
+    }
+
+    router.push(href)
+  }, [router, onClick]);
+
   return (
-    <div className="flex flex-row items-center">
+    <div onClick={handleClick} className="flex flex-row items-center">
       <div className="relative flex items-center justify-center p-4 rounded-full cursor-pointer h-14 w-14 hover:bg-slate-300 hover:bg-opacity-10 lg:hidden">
         <Icon size={28} color="white" />
       </div>
