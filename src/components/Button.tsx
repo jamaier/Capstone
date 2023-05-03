a6e25edc2881ff850422094d9f423e0d63a97ed7
Jacob Maier <jacobamaier@gmail.com>
2023-05-03 14:32:25 -0700

    fix button styling and label display

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,12 +8,42 @@ interface ButtonProps {
   outline?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onClick, disabled }) => {
-  return (
-    <button disabled={disabled} onClick={onClick}>
+const Button: React.FC<ButtonProps> = ({ 
+  label, 
+  secondary, 
+  fullWidth, 
+  onClick, 
+  large, 
+  disabled, 
+  outline 
+}) => {
+  return ( 
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      className={`
+        disabled:opacity-70
+        disabled:cursor-not-allowed
+        rounded-full
+        font-semibold
+        hover:opacity-80
+        transition
+        border-2
+        ${fullWidth ? 'w-full' : 'w-fit'}
+        ${secondary ? 'bg-white' : 'bg-sky-500'}
+        ${secondary ? 'text-black' : 'text-white'}
+        ${secondary ? 'border-black' : 'border-sky-500'}
+        ${large ? 'text-xl' : 'text-md'}
+        ${large ? 'px-5' : 'px-4'}
+        ${large ? 'py-3' : 'py-2'}
+        ${outline ? 'bg-transparent' : ''}
+        ${outline ? 'border-white' : ''}
+        ${outline ? 'text-white' : ''}
+      `}
+    >
       {label}
     </button>
-  );
-};
-
+   );
+}
+ 
 export default Button;
