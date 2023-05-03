afd3462148727bc41c87ac860174092ce2bb793f
Jacob Maier <jacobamaier@gmail.com>
2023-05-02 19:57:22 -0700

    Add button component with styling

---
+++ src/components/Button.tsx
@@ -0,0 +1,27 @@
+interface ButtonProps {
+  label: string;
+  secondary?: boolean;
+  fullWidth?: boolean;
+  large?: boolean;
+  onClick: () => void;
+  disabled?: boolean;
+  outline?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({ 
+  label,
+  onClick,
+  disabled,
+
+}) => {
+  return (
+    <button
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+   );
+}
+ 
+export default Button;
