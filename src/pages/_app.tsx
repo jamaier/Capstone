05d6ee74a3a35f8ef0beca64f26d0be5bf30ef02
Jacob Maier <jacobamaier@gmail.com>
2023-05-05 13:12:57 -0700

    BUGFIX correct db connection

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,8 @@ import { SessionProvider } from 'next-auth/react';
 
 import '@/styles/globals.css';
 import Layout from '@/components/Layout';
-import LoginModal from '@/components/Modals/LoginModal';
-import RegisterModal from '@/components/Modals/RegisterModal';
+import LoginModal from '@/components/modals/LoginModal';
+import RegisterModal from '@/components/modals/RegisterModal';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
