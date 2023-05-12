31bac68501914ccf656f4c680378ba6040dbe92a
Jacob Maier <jacobamaier@gmail.com>
2023-05-12 14:47:37 -0700

    Add more future logic.

--- src/components/posts/PostItem.tsx
+++ src/components/posts/PostItem.tsx
@@ -1,8 +1,10 @@
-import React, { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import { useRouter } from 'next/router';
+import { formatDistanceToNowStrict } from 'date-fns';
 
 import useLoginModal from '@/hooks/useLoginModal';
 import useCurrentUser from '@/hooks/useCurrentUser';
+import { log } from 'console'; // Auto-imported for some reason? will look into why.
 
 interface PostItemProps {
   data: Record<string, any>;
@@ -20,6 +22,24 @@ export const PostItem: React.FC<PostItemProps> = ({ data, userId }) => {
     event.stopPropagation(); // again, this is to prevent the onClick from bubbling up to the parent element
     router.push(`/users/${data.user.id}`)
   }, [router, data.user.id]);
+
+  const goToPost = useCallback(() => {
+    router.push(`/posts/${data.id}`);
+  }, [router, data.id]); // data.id is the post id
+
+  const onLike = useCallback(async (event: any) => {
+    event.stopPropogation();
+
+    loginModal.onOpen();
+  }, [loginModal]);
+
+  const createdAt = useMemo(() => {
+    if (!data?.createdAt) {
+      return null;
+    }
+
+    return formatDistanceToNowStrict(new Date(data.createdAt));
+  }, [data.createdAt]);
 
   return (
     <div>PostItem</div>
