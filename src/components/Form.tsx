e077bb8f19cb1eec360e2c431c635ec37b959042
Jacob Maier <jacobamaier@gmail.com>
2023-05-12 14:08:08 -0700

    Add Post form for user and test post.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,6 +8,7 @@ import useCurrentUser from '@/hooks/useCurrentUser';
 import usePosts from '@/hooks/usePosts';
 
 import Button from '@/components/Button';
+import Avatar from '@/components/Avatar';
 
 interface FormProps {
   placeholder: string;
@@ -44,16 +45,38 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
   return (
     <div className="border-b-[1px] border-neutral-800 px-5 py-2">
       {currentUser ? (
-        <div></div>) : (
-      <div className="py-8">
-        <h1 className="mb-4 text-2xl font-bold text-center text-white">
-          welcome to the social app
-        </h1>
-        <div className='flex flex-row items-center justify-center gap-4'>
-          <Button label='Login' onClick={loginModal.onOpen} />
-          <Button label='Register' onClick={registerModal.onOpen} secondary />
+        <div className="flex flex-row gap-4">
+          <div>
+            <Avatar userId={currentUser?.id} />
+          </div>
+          <div className="w-full">
+            <textarea
+              disabled={isLoading}
+              onChange={(e) => setBody(e.target.value)}
+              value={body}
+              className="w-full mt-3 bg-black outline-none resize-none disabled:opacity-80 peer ring-0 text-[20px] placeholder:neutral-500 text-white"
+              placeholder={placeholder}
+            ></textarea>
+            <hr className="opacity-0 peer-focus:opacity-100 h-[1px] w-full border-neutral-800 transition" />
+            <div className="flex flex-row justify-end mt-4">
+              <Button
+                disabled={isLoading || !body}
+                onClick={onSubmit}
+                label="Post"
+              />
+            </div>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="py-8">
+          <h1 className="mb-4 text-2xl font-bold text-center text-white">
+            welcome to the social app
+          </h1>
+          <div className="flex flex-row items-center justify-center gap-4">
+            <Button label="Login" onClick={loginModal.onOpen} />
+            <Button label="Register" onClick={registerModal.onOpen} secondary />
+          </div>
+        </div>
       )}
     </div>
   );
