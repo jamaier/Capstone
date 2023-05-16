63bace417066736e388dd8b633f7e0de85608290
Jacob Maier <jacobamaier@gmail.com>
2023-05-15 18:18:14 -0700

    WIP Following users functionality not working

--- src/components/users/UserBio.tsx
+++ src/components/users/UserBio.tsx
@@ -1,10 +1,10 @@
-import { format } from 'date-fns';
 import { useMemo } from 'react';
-
 import { BiCalendar } from 'react-icons/bi';
+import { format } from 'date-fns';
 
 import useCurrentUser from '@/hooks/useCurrentUser';
 import useUser from '@/hooks/useUser';
+import useFollow from '@/hooks/useFollow';
 import useEditModal from '@/hooks/useEditModal';
 
 import Button from '../Button';
@@ -18,6 +18,8 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
   const { data: fetchedUser } = useUser(userId);
 
   const editModal = useEditModal();
+
+  const { isFollowing, toggleFollow } = useFollow(userId);
 
   const createdAt = useMemo(() => {
     if (!fetchedUser?.createdAt) {
@@ -33,7 +35,12 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
         {currentUser?.id === userId ? (
           <Button secondary label="Edit" onClick={editModal.onOpen} />
         ) : (
-          <Button secondary label="Follow" onClick={() => {}} />
+          <Button
+            onClick={toggleFollow}
+            label={isFollowing ? 'Unfollow' : 'Follow'}
+            secondary={!isFollowing}
+            outline={isFollowing}
+          />
         )}
       </div>
       <div className="px-4 mt-8">
@@ -44,30 +51,20 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
           <p className="text-md text-neutral-500">{fetchedUser?.username}</p>
         </div>
         <div className="flex flex-col mt-4">
-          <p className="text-white">
-            {fetchedUser?.bio}
-          </p>
+          <p className="text-white">{fetchedUser?.bio}</p>
           <div className="flex flex-row items-center gap-2 mt-4 text-neutral-500">
             <BiCalendar size={24} />
             <p>Joined {createdAt}</p>
           </div>
         </div>
-        <div className='flex flex-row items-center gap-6 mt-4'>
-          <div className='flex flex-col items-center gap-1'>
-            <p className='text-white'>
-              {fetchedUser?.followingIds?.length}
-            </p>
-            <p className='text-neutral-500'>
-              Following
-            </p>
+        <div className="flex flex-row items-center gap-6 mt-4">
+          <div className="flex flex-row items-center gap-1">
+            <p className="text-white">{fetchedUser?.followingIds?.length}</p>
+            <p className="text-neutral-500">Following</p>
           </div>
-          <div className='flex flex-col items-center gap-1'>
-            <p className='text-white'>
-              {fetchedUser?.followersCount || 0}
-            </p>
-            <p className='text-neutral-500'>
-              Followers
-            </p>
+          <div className="flex flex-row items-center gap-1">
+            <p className="text-white">{fetchedUser?.followersCount || 0}</p>
+            <p className="text-neutral-500">Followers</p>
           </div>
         </div>
       </div>
--- src/pages/api/users/follow.ts
+++ src/pages/api/users/follow.ts
@@ -1,13 +1,13 @@
-import serverAuth from "@/libs/serverAuth";
 import { NextApiRequest, NextApiResponse } from "next";
 
-import prisma from "@/libs/prismadb";
+import prisma from '@/libs/prismadb';
+import serverAuth from '@/libs/serverAuth';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "POST" && req.method !== "DELETE") {
+  if (req.method !== 'POST' && req.method !== 'DELETE') {
     return res.status(405).end();
   }
 
@@ -16,25 +16,27 @@ export default async function handler(
 
     const { currentUser } = await serverAuth(req, res);
 
-    if (!userId || typeof userId !== "string") {
-      throw new Error("Invalid ID");
+    if (!userId || typeof userId !== 'string') {
+      throw new Error('Invalid ID');
     }
 
     const user = await prisma.user.findUnique({
-      where: { id: userId },
+      where: {
+        id: userId
+      }
     });
 
     if (!user) {
-      throw new Error("Invalid ID");
+      throw new Error('Invalid ID');
     }
 
     let updatedFollowingIds = [...(user.followingIds || [])];
 
-    if (req.method === "POST") {
+    if (req.method === 'POST') {
       updatedFollowingIds.push(userId);
     }
 
-    if (req.method === "DELETE") {
+    if (req.method === 'DELETE') {
       updatedFollowingIds = updatedFollowingIds.filter((followingId) => followingId !== userId);
     }
 
@@ -49,7 +51,7 @@ export default async function handler(
 
     return res.status(200).json(updatedUser);
   } catch (error) {
-    console.error(error);
+    console.log(error);
     return res.status(400).end();
   }
 }
