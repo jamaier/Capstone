565a5e8edcc83b46096c9213a1e3f54fc7aad8db
Jacob Maier <jacobamaier@gmail.com>
2023-05-17 10:58:10 -0700

    Add key for notifications, styling, and formatter

--- src/components/NotificationsFeed.tsx
+++ src/components/NotificationsFeed.tsx
@@ -1,8 +1,8 @@
-import useCurrentUser from "@/hooks/useCurrentUser";
-import useNotifications from "@/hooks/useNotifications";
-import { FaDev } from "react-icons/fa";
+import { FaDev } from 'react-icons/fa';
 
-import { useEffect } from "react";
+import useNotifications from '@/hooks/useNotifications';
+import useCurrentUser from '@/hooks/useCurrentUser';
+import { useEffect } from 'react';
 
 const NotificationsFeed = () => {
   const { data: currentUser, mutate: mutateCurrentUser } = useCurrentUser();
@@ -17,21 +17,22 @@ const NotificationsFeed = () => {
       <div className="p-6 text-xl text-center text-neutral-600">
         No notifications
       </div>
-    )
+    );
   }
 
   return (
     <div className="flex flex-col">
       {fetchedNotifications.map((notification: Record<string, any>) => (
-        <div className="flex flex-row items-center gap-4 p-6 border-b-[1px] border-neutral-800">
-          <FaDev color='white' size={32} />
-          <p className="text-white">
-            {notification.body}
-          </p>
+        <div
+          key={notification.id}
+          className="flex flex-row items-center p-6 gap-4 border-b-[1px] border-neutral-800"
+        >
+          <FaDev color="white" size={32} />
+          <p className="text-white">{notification.body}</p>
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
 export default NotificationsFeed;
