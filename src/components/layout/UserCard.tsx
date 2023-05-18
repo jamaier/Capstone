ce6788a7072c4795426de134dfefac7e7ccc5907
Jacob Maier <jacobamaier@gmail.com>
2023-05-18 12:42:52 -0700

    update casing

---
+++ src/components/layout/UserCard.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image';
+
+import useCurrentUser from '@/hooks/useCurrentUser';
+
+import Avatar from '../Avatar';
+
+interface UserCardProps {
+  userId?: string;
+}
+
+const UserCard: React.FC<UserCardProps> = ({ userId }) => {
+  const { data: currentUser } = useCurrentUser();
+
+  if (!currentUser) {
+    return null;
+  }
+
+  return (
+    <div>
+      <div className="relative hidden bg-neutral-700 h-44 lg:block">
+        {currentUser?.coverImage && (
+          <Image
+            src={currentUser.coverImage}
+            alt="Cover Image"
+            fill
+            style={{ objectFit: 'cover' }}
+          />
+        )}
+        <div className="absolute -bottom-[9rem] left-8">
+          {' '}
+          {/* // (-)bottom appends to override the default bottom value   */}
+          <Avatar userId={currentUser?.id} isLarge hasBorder />
+          <div className="flex flex-col gap-2 mt-4">
+            <p className="text-xl font-semibold text-white">
+              {currentUser?.name}
+            </p>
+            <p className="text-sm text-neutral-400">{currentUser?.username}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
