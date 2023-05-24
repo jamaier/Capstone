fe051bb300edc5a4c2254b971b01d47a94fadd39
Jacob Maier <jacobamaier@gmail.com>
2023-05-24 11:16:20 -0700

    BUGFIX: Add pagination to main feed This is top fix call size breaking main feed after approx 20 posts.

--- src/components/posts/PostFeed.tsx
+++ src/components/posts/PostFeed.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import usePosts from '@/hooks/usePosts';
 
 import PostItem from './PostItem';
@@ -9,11 +10,39 @@ interface PostFeedProps {
 const PostFeed: React.FC<PostFeedProps> = ({ userId }) => {
   const { data: posts = [] } = usePosts(userId);
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 10;
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <>
-      {posts.slice(0, 15).map((post: Record<string, any>) => (
+      {currentPosts.map((post: Record<string, any>) => (
         <PostItem userId={userId} key={post.id} data={post} />
       ))}
+
+      {/* Pagination controls */}
+      <div className="flex pl-2 mt-4 space-x-2">
+        {Array.from({ length: Math.ceil(posts.length / postsPerPage) }).map((_, index) => (
+          <button
+            key={index}
+            onClick={() => handlePageChange(index + 1)}
+            className={`px-4 py-2 rounded-full ${
+              currentPage === index + 1
+                ? 'bg-green-500 text-white'
+                : 'bg-white text-black'
+            }`}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </>
   );
 };
